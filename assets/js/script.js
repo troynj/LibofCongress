5ead09c7c5ca53608865b94bbe2ca8844c2f9186
DBENSAN <dftb97@gmail.com>
2023-01-10 15:54:50 -0800

    Sets fetch URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,49 @@ var format = "";
 var userInput = "";
 var locUrl = `https://www.loc.gov/${format}/?q=${userInput}&fo=json`
 
+
+function setFormat() {
+    var userDrop = "";
+    switch (getFormat()) {
+        case 'Maps': userDrop = "maps"
+            break;
+        case 'Audio Recordings': userDrop = "audio"
+            break;
+        case 'Photo, print, drawings': userDrop = "photos"
+            break;
+        case 'Manuscripts / Mixed Material': userDrop = "manuscripts"
+            break;
+        case 'Newspapers': userDrop = "newspapers"
+            break;
+        case 'Film, Video': userDrop = "film-and-videos"
+            break;
+        case 'Printed Music, such as Sheet Music': userDrop = "notated-music"
+            break;
+        case 'Archived Websites': userDrop = "websites"
+            break;
+        default: alert("Please make a selection.");
+            break;
+    } return userDrop
+}
+
+function setUserInput() {
+    var tempUserInput = getUserInput();
+    var userInputArr = tempUserInput.split(" ");
+    console.log(typeof userInputArr);
+    console.log(userInputArr);
+    var userInputStr = userInputArr.join('')
+    console.log(userInputStr)
+    return userInputStr
+}
+
 function setSearch() {
-    format = getFormat();
-    userInput = getUserInput();
+    format = setFormat();
+    userInput = setUserInput();
 }
 
 function getFormat() {
-    return $('#format').selectmenu()
+    return $('#format').selectmenu().val()
+
 }
 
 function getUserInput() {
@@ -17,17 +53,22 @@ function getUserInput() {
 }
 
 $('#search-btn').click(() => {
-setSearch()
+    setSearch()
 
-fetch(locUrl)
-.then((response) => {
-    //if statement to check response.status
-    return response.json()
-})
-.then((data) => {
-    console.log(data)
-    //append data to page
-})}
+    console.log(format)
+    console.log(userInput)
+    console.log(`https://www.loc.gov/${format}/?q=${userInput}&fo=json`);
+
+    fetch(locUrl)
+        .then((response) => {
+            //if statement to check response.status
+            return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            //append data to page
+        })
+}
 )
 
 //function to dynamically generate divs with data 
